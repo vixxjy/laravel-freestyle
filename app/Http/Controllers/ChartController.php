201e64e760d64d53cf3a2817cd80a76e2550ca03
smilez <ajorvictor48@gmail.com>
2017-08-09 10:28:19 +0000

    first commit

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use DB;
+use Charts;
+
+class ChartController extends Controller
+{
+    public function index(){
+        $users = User::where(DB::raw("(DATE_FORMAT(created_at, '%Y'))"), date('Y'))->get();
+        
+        $chart = Charts::database($users, 'bar', 'highcharts')->title("Monthly new Register Users")
+        ->elementLabel("Total Users")
+        ->dimensions(1000, 500)
+        ->responsive(true)
+        ->groupByMonth(date('Y'), true);
+        
+        return view('charts', compact('chart'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', 'ChartController@index');
+
+Route::resource('posts', 'PostsController');
